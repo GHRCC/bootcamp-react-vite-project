85940b75720a09a5f31a9638682fbf0e3d275bfe
Guilherme Cardim <guilherme.rccarvalho@al.infnet.edu.br>
2023-03-21 13:57:04 -0300

    Created app bar

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,13 +2,30 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { SiNotepadplusplus } from "react-icons/si";
+import { Link } from "react-router-dom";
 
 function Home() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <h1 className="bg-red-500 text-pink-700">Hello</h1>
+    <div>
+      <div className="bg-teal-400 h-16 flex items-center px-8">
+        <div className="flex items-center gap-2">
+          <Link to="/" className="group">
+            <SiNotepadplusplus
+              size="70px"
+              className="text-red-600 group-hover:scale-110"
+            />
+          </Link>
+          <Link
+            to="/"
+            className="text-red-600 font-semibold font-serif text-xl hover:text-2xl border-b-2 border-red-600 "
+          >
+            Notepads
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
